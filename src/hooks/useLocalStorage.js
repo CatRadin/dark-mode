c59684d41817e1d3db1e8fa27d12851a6ba0763f
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-10 15:55:18 -0600

    MVP

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,24 @@
-import { useState } from 'react';
-
-export const useLocalStorage = (key, initalValue) {
-
-//State-------------------------------------------------------
-const [storedValue, setStoredValue] = useState()
+import { useState } from 'react'
 
 
+
+ export const useLocalStorage = (key, initialValue) => {
+
+//State------------------------------------------------------------
+const [storedValue, setStoredValue] = useState(() => {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key);
+    // Parse and return stored json or, if undefined, return initialValue
+    return item ? JSON.parse(item) : initialValue;
+  });
+//Setter Function-------------------------------------------------
+const setValue = value => {
+    //Update State--
+    setStoredValue(value)
+    //Update Local Storage--
+    window.localStorage.setItem(key, JSON.stringify(value))
+}
+//Value returned from Local Storage (dark mode in this case)------------------
+  return [storedValue, setValue]
 }
 
